2396220394571e0d57c0e8144f5ec1eff6d1afcd
Keith Russo <keith.russo@veteransoftware.us>
2022-03-02 21:29:40 -0500

    fix: remove settings input from main.go

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ package main
 import (
 	_ "github.com/veteran-software/discord-api-wrapper/api"
 	_ "github.com/veteran-software/discord-api-wrapper/logging"
-	_ "github.com/veteran-software/discord-api-wrapper/settings"
 )
 
 func main() {
