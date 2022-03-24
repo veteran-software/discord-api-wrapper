97a0c1ca57e0afe0c7df4fcff611bd2656c34824
Keith Russo <keith.russo@veteransoftware.us>
2022-03-24 09:59:59 -0400

    feat: add localization options in application commands
    closes #1
    
    Signed-off-by: Keith Russo <keith.russo@veteransoftware.us>

--- api/application_commands.go
+++ api/application_commands.go
@@ -94,7 +94,7 @@ type ApplicationCommandOptionChoice struct {
 
 // ApplicationCommandInteractionDataOption - All options have names, and an option can either be a parameter and input value--in which case value will be set--or it can denote a subcommand or group--in which case it will contain a top-level key and another array of options.
 //
-// value and options are mutually exclusive.
+//     value and options are mutually exclusive.
 type ApplicationCommandInteractionDataOption struct {
 	Name    string                                     `json:"name"`              // the name of the parameter
 	Type    ApplicationCommandOptionType               `json:"type"`              // value of application command option type
